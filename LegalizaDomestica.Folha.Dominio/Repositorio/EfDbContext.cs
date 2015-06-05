c387a30afd40b785445fcc96c7ba269cdc26a14c
jsaleandro <aleandro.silva@mgasolutions.com.br>
2015-06-05 20:51:00 -0300

    Inclusao do Mysql

---
+++ LegalizaDomestica.Folha.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegalizaDomestica.Folha.Dominio.Repositorio
+{
+   public class EfDbContext : DbContext
+
+    {
+
+    }
+}
